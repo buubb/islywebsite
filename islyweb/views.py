858d2d392dcbdf8f75c2ba50135890ad9b863ab4
buubb <qls773322@naver.com>
2023-07-17 02:18:27 +0900

    230717 main 페이지 세팅

---
+++ islyweb/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+class Main(APIView):
+    def get(self, request):
+        print("get으로 호출")
+        return render(request, 'islyweb/main.html')
+    
+    def post(self, request):
+        print("post로 호출")
+        return render(request, 'islyweb/main.html')
