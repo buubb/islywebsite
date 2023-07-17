0393fb4f93a01df21bdb726e7059928affc337ac
buubb <qls773322@naver.com>
2023-07-17 22:39:54 +0900

    assignments url 수정 230717

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AssignmentsList, AssignmentsSubmit
+from .views import AdvancedList, Submit, BasicList
 
 urlpatterns = [
-    path('list/', AssignmentsList.as_view()),
-    path('submit/', AssignmentsSubmit.as_view())
+    path('advanced/', AdvancedList.as_view()),
+    path('basic/', BasicList.as_view()),
+    path('submit/', Submit.as_view())
 ]
--- assignments/views.py
+++ assignments/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
-class AssignmentsList(APIView):
+class BasicList(APIView):
     def get(self, request):
         print("get으로 호출")
-        return render(request, 'assignments/list.html')
+        return render(request, 'assignments/basiclist.html')
     
     def post(self, request):
         print("post로 호출")
-        return render(request, 'assignments/list.html')
+        return render(request, 'assignments/basiclist.html')
     
-class AssignmentsSubmit(APIView):
+class AdvancedList(APIView):
+    def get(self, request):
+        print("get으로 호출")
+        return render(request, 'assignments/advancedlist.html')
+    
+    def post(self, request):
+        print("post로 호출")
+        return render(request, 'assignments/advancedlist.html')
+
+class Submit(APIView):
     def get(self, request):
         print("get으로 호출")
         return render(request, 'assignments/submit.html')
