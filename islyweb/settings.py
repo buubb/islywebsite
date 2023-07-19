6843d75623d850076a7ec5e5d593daf1772908cd
buubb <qls773322@naver.com>
2023-07-19 21:46:36 +0900

    네비바 생성 및 연결

--- islyweb/settings.py
+++ islyweb/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# 기능(어플) 추가 시 아래에 이름을 추가해주세요. 하위 기능은 추가 XX
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
