5076c9c73d03ba532e5b9c38d13914a264869174
Sozero_00 <rabbit6805@gmail.com>
2023-08-26 20:52:38 +0900

    soyoung

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Library
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+@admin.register(Library)
+class LibraryAdmin(SummernoteModelAdmin) :
+    summernote_fields = ('contents',)
+    list_display = (
+        'title',
+        'contents',
+        'writer',
+        'write_dttm',
+        'update_dttm'
+    )
+    list_display_links = list_display
