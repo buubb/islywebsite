e51b60a28bb1a18d60a704bc4a50dcbfdf787d12
enjprk41 <enjprk41@gmail.com>
2023-08-26 13:50:27 +0900

    third commit

--- assignments/models.py
+++ assignments/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-# 게시글(Post)엔 제목(title), 내용(content)이 존재합니다
+# 게시글(Post)엔 제목(title), 내용(content)이 존재합니다.
+
 class BasicAssignment(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
