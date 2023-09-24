e891fa6a9de18b7abbfea16d5164831a27ed4ab7
Sozero_00 <rabbit6805@gmail.com>
2023-09-24 11:38:41 +0900

    soyoung0924

--- library/models.py
+++ library/models.py
@@ -1,20 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# 게시글(Post)엔 제목(title), 내용(content)이 존재합니다.
 
-class Library(models.Model):
-    title = models.CharField(max_length=64, verbose_name= '글 제목')
-    contents = models.TextField(verbose_name='글 내용')
-    writer = models.ForeignKey('User.User', on_delete=models.CASCADE, verbose_name='작성자')
-    writer_dttm = models.DateTimeField(auto_now_add=True, verbose_name='글 작성일')
-    
-    updata_dttm = models.DateTimeField(auto_now=True, verbose_name='마지막 수정일')
-    
+class Post(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
-    
-    class Meta:
-        db_table = 'library'
-        verbose_name = '게시판'
-        verbose_name_plural = '게시판'
+        return self.subject
+#복사본
