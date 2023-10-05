f0da85d2ef65c31ba15d689c8afeff07f4291df3
SUMI LEE <dltnal339@naver.com>
2023-10-05 19:58:36 +0900

    10.05

--- recruit/views.py
+++ recruit/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 class Recruit(APIView):
     def get(self, request):
         print("get으로 호출")
-        return render(request, 'recruit/tem2.html')
+        return render(request, 'recruit/b.html')
     
     def post(self, request):
         print("post로 호출")
-        return render(request, 'recruit/tem2.html')
+        return render(request, 'recruit/b.html')
