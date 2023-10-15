859920c68ec575b3eca92b7ec303e65b72154d2d
buubb <qls773322@naver.com>
2023-10-15 17:39:10 +0900

    [유빈]team페이지 백엔드 수정

--- team_activities/models.py
+++ team_activities/models.py
@@ -1,3 +1,20 @@
+import os
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class TeamActivitiesPost(models.Model):
+    title = models.CharField(max_length=200, verbose_name='제목')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='작성자')
+    content=models.TextField(verbose_name='내용')
+    hits=models.PositiveIntegerField(verbose_name='조회수',default=0)
+    registered_date=models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
+    top_fixed=models.BooleanField(verbose_name='상단고정', default=False)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = 'teampost'
+        verbose_name='teampost'
+        verbose_name_plural='teampost'
