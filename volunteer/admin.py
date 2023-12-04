b1a7b292869536f6f9c7ac87edd49403731e44a3
raon <raon-yang@naver.com>
2023-12-04 12:19:18 +0900

    7-added-raon

--- volunteer/admin.py
+++ volunteer/admin.py
@@ -42,7 +42,7 @@ class PostAdmin(admin.ModelAdmin):
         "year",
         "participant",
         "content",
-        "created"
+        "created",
     ]
     inlines = [
         CommentInline,
