2810365dcf2dc3ee412c779305127e6d560a449c
raon <raon-yang@naver.com>
2023-12-16 23:42:35 +0900

    [raon] 8-added/ modified detail page

--- volunteer/views.py
+++ volunteer/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from volunteer.models import Post
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 def feed(request):
@@ -20,27 +21,29 @@ def feed(request):
 
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
-    print(post)
+    all_posts = Post.objects.exclude(id=post_id).order_by('-created')
 
     context = {
         "post": post,
+        "all_posts": all_posts,
     }
     return render(request, "volunteer/post_detail.html", context)
 
 
+@login_required(login_url="login")
 def post_like(request, post_id):
     post = Post.objects.get(id=post_id)
     user = request.user
 
-    if not user.is_authenticated:
-        return HttpResponseRedirect(reverse("login") + f"?next={request.path}")
-
+    # 사용자가 "좋아요를 누른 Post 목록"에 "좋아요 버튼을 누른 Post"가 존재한다면
     if user.like_posts.filter(id=post.id).exists():
+        # 좋아요 목록에서 삭제한다
         user.like_posts.remove(post)
+    # 존재하지 않는다면 좋아요 목록에 추가한다
     else:
         user.like_posts.add(post)
 
-    page_number = request.GET.get("page")
+    # next로 값이 전달되었다면 해당 위치로, 전달되지 않았다면 해당 Post 위치로 이동한다
+    url_next = request.GET.get("next") or reverse("Volunteer:post_detail", args=[post_id])
 
-    url_next = request.GET.get("next") or reverse("Volunteer:feed") + f"?page={page_number}#post-{post.id}"
     return HttpResponseRedirect(url_next)
