ffc9016d92eacb8a5957ae3762b76f4e6b37b9cd
buubb <qls773322@naver.com>
2024-02-03 16:41:51 +0900

    [yubin] main page menu

--- static/fullpage/main.js
+++ static/fullpage/main.js
@@ -4,9 +4,9 @@ $(function(){
         anchors:['s1','s2','s3','s4','s5'],
 		//autoScrolling:true,
 		//scrollHorizontally: true,
-        navigation: true,
-        navigationPosition: 'bottom',
-        sectionsColor:['#1f2122','#27292a','#1f2122','#27292a', '#1f2122']
+        //navigation: true,
+        //navigationPosition: 'bottom',
+        sectionsColor:['#1f2122','#1f2122','#1f2122','#1f2122', '#1f2122']
 	});
 });
 
