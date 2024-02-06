0c79d74d21e3c688e7e1faf183ee284d4feb4e7c
buubb <qls773322@naver.com>
2024-02-06 20:02:43 +0900

    yubin main page

--- static/fullpage/main.js
+++ static/fullpage/main.js
@@ -19,3 +19,31 @@ function toggleAnswer(answerID) {
       answerDiv.style.display = 'none';
     }
   }
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const content1 = "Hello, We are\n";
+    const content2 = "I.Sly()";
+    const text = document.querySelector(".text");
+    const cursor = document.querySelector(".cursor"); // 커서 요소 선택
+    let i = 0;
+
+    function typing() {
+        let txt = i < content1.length ? content1[i] : content2[i - content1.length];
+        if (i >= content1.length) {
+            text.insertAdjacentHTML('beforeend', `<span class="green">${txt}</span>`);
+        } else {
+            text.innerHTML += txt === "\n" ? "<br/>" : txt;
+        }
+        i++;
+        if (i < content1.length + content2.length) {
+            setTimeout(typing, 200);
+        } else {
+            setTimeout(function() {
+                cursor.style.display = 'none'; // 3초 후에 커서 숨김
+            }, 3000); // 3초 후에 실행
+        }
+    }
+
+    typing(); // 처음 한 번만 실행
+});
