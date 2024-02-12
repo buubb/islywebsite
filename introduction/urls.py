2a53333dc6c7318628b7dbbf9d96ffe6e7cf6dab
raon <raon-yang@naver.com>
2024-02-13 04:30:47 +0900

    [raon] 0213 introduction

--- introduction/urls.py
+++ introduction/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import Introduction
+# from .views import Introduction
+from .views import introduction, rules
 
+app_name = "Introduction"
 urlpatterns = [
     # url path: introduction/
-    path('', Introduction.as_view()),
+    # path('', Introduction.as_view()),
+    path("", introduction, name="introduction"),
+    path("rules/", rules, name="rules"),
 ]
