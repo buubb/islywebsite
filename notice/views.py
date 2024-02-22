4e9d104ffba06b2e6e18efecbdcfaa679008dbd0
raon <raon-yang@naver.com>
2024-02-22 11:54:19 +0900

    [raon] 0222 notice list

--- notice/views.py
+++ notice/views.py
@@ -5,8 +5,8 @@ from rest_framework.views import APIView
 class Notice(APIView):
     def get(self, request):
         print("get으로 호출")
-        return render(request, 'notice/a.html')
+        return render(request, 'notice/notice.html')
     
     def post(self, request):
         print("post로 호출")
-        return render(request, 'notice/a.html')
+        return render(request, 'notice/notice.html')
