038a8a75f83ae44460c72bfbf4fe114cced2eff0
sumi-2 <dltnal339@naver.com>
2024-03-05 20:06:49 +0900

    3.5 first commit

--- notice/views.py
+++ notice/views.py
@@ -1,12 +1,19 @@
 # backend
 from django.shortcuts import render
 from rest_framework.views import APIView
+from .models import AttendanceLink, SubmissionLink, RentalLink
 
 class Notice(APIView):
     def get(self, request):
         print("get으로 호출")
-        return render(request, 'notice/notice.html')
-    
+        attendance_link = AttendanceLink.objects.first()
+        submission_link = SubmissionLink.objects.first()
+        rental_link = RentalLink.objects.first()  
+        return render(request, 'notice/notice.html', {'attendance_link': attendance_link, 'submission_link': submission_link, 'rental_link': rental_link})
+
     def post(self, request):
         print("post로 호출")
-        return render(request, 'notice/notice.html')
+        attendance_link = AttendanceLink.objects.first()
+        submission_link = SubmissionLink.objects.first()
+        rental_link = RentalLink.objects.first()  
+        return render(request, 'notice/notice.html', {'attendance_link': attendance_link, 'submission_link': submission_link, 'rental_link': rental_link})
