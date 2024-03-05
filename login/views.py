f38bae06c699306a53dbc7ca1e64c885950ed446
enjprk41 <enjprk41@gmail.com>
2024-03-05 17:04:12 +0900

    240305 commit

--- login/views.py
+++ login/views.py
@@ -22,16 +22,15 @@ class Login(APIView):
     
     def post(self, request):
         print("post로 호출")
-        form=AuthenticationForm(request=request, data=request.POST)
+        form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
-            username=form.cleaned_data.get('username')
-            password=form.cleaned_data.get('password')
-            user=authenticate(username=username, password=password)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
             
             if user is not None:
                 print(user)
                 user_instance = get_user_model().objects.get(username=username)
-
                 LoginFail.objects.filter(user__username=username).delete()
                     
                 login(request, user)
---
+++ static/recruit/js/apply.js
@@ -0,0 +1,12 @@
+// apply.js 파일에 checkRecruitment 함수를 정의합니다.
+function checkRecruitment() {
+    var currentDateTime = new Date();
+    var recruitmentStart = new Date('2024-01-27'); // 모집 시작 날짜
+    var recruitmentEnd = new Date('2024-02-15');   // 모집 종료 날짜
+
+    if (currentDateTime >= recruitmentStart && currentDateTime <= recruitmentEnd) {
+        window.location.href = "https://www.naver.com"; // 지원 링크로 이동
+    } else {
+        alert("지금은 모집 기간이 아닙니다");
+    }
+}
