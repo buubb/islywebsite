b022ecbf24f8a20f4b36558f9e09d370e25785a9
Eunju Park <122546382+enjprk41@users.noreply.github.com>
2024-03-05 19:46:03 +0900

    Revert "240305 push - recruit 페이지 수정 (1)"

--- recruit/views.py
+++ recruit/views.py
@@ -1,8 +1,9 @@
 # backend
 from django.shortcuts import render
 
+
 def recruit(request):
-    return render(request, 'recruit/recruit.html')
+    return render(request, "recruit/recruit.html")
 
 def status(request):
     return render(request, "recruit/status.html")
