6f104fa35543e27aee2b130a04e21e0ec96e1e43
buubb <qls773322@naver.com>
2024-03-09 19:45:46 +0900

    [yubin] Update Color, Main

--- static/fullpage/selected.js
+++ static/fullpage/selected.js
@@ -3,7 +3,7 @@ $(function(){
     var url = window.location.href; 
 
     // passes on every "a" tag 
-    $(".nav a").each(function() {
+    $(".nav .menu_name").each(function() {
             // checks if its the same on the address bar
         if(url == (this.href)) { 
             $(this).closest(".menu_li").addClass("active");
