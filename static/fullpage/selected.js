02ffc1224a9456333d3ccc922c011af7f389c9ce
buubb <qls773322@naver.com>
2024-03-09 20:20:43 +0900

    [yubin] Update Nav

--- static/fullpage/selected.js
+++ static/fullpage/selected.js
@@ -3,7 +3,7 @@ $(function(){
     var url = window.location.href; 
 
     // passes on every "a" tag 
-    $(".nav .menu_name").each(function() {
+    $(".nav a").each(function() {
             // checks if its the same on the address bar
         if(url == (this.href)) { 
             $(this).closest(".menu_li").addClass("active");
