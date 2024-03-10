705e5418703804741c0bac8cd587edc87c555f65
enjprk41 <enjprk41@gmail.com>
2024-03-11 02:01:28 +0900

    240311 commit

--- static/recruit/js/apply.js
+++ static/recruit/js/apply.js
@@ -1,5 +1,7 @@
 // apply.js 파일에 checkRecruitment 함수를 정의합니다.
 function checkRecruitment() {
+    event.preventDefault();
+
     var currentDateTime = new Date();
     var recruitmentStart = new Date('2024-01-24'); // 모집 시작 날짜
     var recruitmentEnd = new Date('2024-02-07');   // 모집 종료 날짜
@@ -10,4 +12,3 @@ function checkRecruitment() {
         alert("지금은 모집 기간이 아닙니다. 다음 10기 모집을 기다려주세요!");
     }
 }
-event.preventDefault();
