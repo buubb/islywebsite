e93f74798c2f75ff979c77b156cef0c60b232158
raon <raon-yang@naver.com>
2024-03-16 02:41:39 +0900

    [raon] 0316 recruit backend

--- recruit/models.py
+++ recruit/models.py
@@ -1,6 +1,7 @@
 # models.py
 from django.db import models
 
+
 class Recruitment(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
@@ -8,3 +9,11 @@ class Recruitment(models.Model):
 
     def __str__(self):
         return f"Recruitment from {self.start_date} to {self.end_date}"
+
+
+class Announcement(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"Announcement from {self.start_date} to {self.end_date}"
--- recruit/views.py
+++ recruit/views.py
@@ -1,16 +1,24 @@
 # backend
 from django.shortcuts import render
-from .models import Recruitment
+from .models import Recruitment, Announcement
 import datetime
 
 
 def recruit(request):
     recruitment = Recruitment.objects.first()
+    announcement = Announcement.objects.first()
     today = datetime.date.today()
+
+    # 합격 발표 기간
+    if announcement and announcement.start_date <= today <= announcement.end_date:
+        return render(request, "recruit/status.html")
+
+    # 지원 기간
     if recruitment and recruitment.start_date <= today <= recruitment.end_date:
         return render(request, "recruit/recruit.html", {"application_link": recruitment.application_link})
-    else:
-        return render(request, "recruit/recruit.html", {"not_in_recruitment_period": True})
+
+    # 그 외 기간
+    return render(request, "recruit/recruit.html", {"not_in_recruitment_period": True})
 
 
 def status(request):
