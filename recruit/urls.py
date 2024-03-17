d758ad913a09e178152ba7f62e1c47f36ec36b15
raon <raon-yang@naver.com>
2024-03-17 23:41:07 +0900

    [raon] 0317 recruit backend

--- recruit/urls.py
+++ recruit/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import recruit, status
+from .views import recruit
 
 
 app_name = "Recruit"
 urlpatterns = [
     # url path: recruit/
     path("", recruit, name="recruit"),
-    path("status/", status, name="status"),
 ]
