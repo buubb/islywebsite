961bf95017ca98d99665a3fa8e9e7af6c94bb5b9
buubb <qls773322@naver.com>
2024-03-20 23:00:27 +0900

    yubin models.py delete

--- main/models.py
+++ main/models.py
@@ -1,18 +1,1 @@
 from django.db import models
-
-class SpecialActivitiesPost(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    photo = models.ImageField("image",upload_to="images", null=True)
-
-    def __str__(self):
-        return self.title
-    
-class ActivityRecordsCount(models.Model):
-    years = models.IntegerField()
-    applicants = models.IntegerField()
-    members = models.IntegerField()
-    projects = models.IntegerField()
-
-    def __str__(self):
-        return self.years
