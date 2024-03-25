4196f44b8948a616ffca1e1c77b6b521ab81be6d
enjprk41 <enjprk41@gmail.com>
2024-03-25 23:23:42 +0900

    [eunju] recruit backend

--- recruit/views.py
+++ recruit/views.py
@@ -11,19 +11,7 @@ def recruit(request):
 
     # 합격 발표 기간
     if announcement and announcement.start_date <= today <= announcement.end_date:
-        # 전화번호 입력
-        if request.method == 'POST':
-            phone_number = request.POST.get('applicant_phone')
-            try:
-                applicant = Applicant.objects.get(phone_number=phone_number)
-                if applicant.is_passed:
-                    return render(request, "recruit/pass.html")
-                else:
-                    return render(request, "recruit/fail.html")
-            except Applicant.DoesNotExist:
-                return render(request, "recruit/status.html", {"not_in_recruitment_period": True})
-        else:
-            return render(request, "recruit/status.html")
+        return render(request, "recruit/status.html")
 
     # 모집 기간
     if recruitment and recruitment.start_date <= today <= recruitment.end_date:
@@ -31,3 +19,15 @@ def recruit(request):
 
     # 그 외 기간
     return render(request, "recruit/recruit.html", {"not_in_recruitment_period": True})
+
+
+def check_status(request):
+    phone_number = request.POST.get('applicant_phone')
+    try:
+        applicant = Applicant.objects.get(phone_number=phone_number)
+        if applicant.is_passed:
+            return render(request, 'recruit/pass.html')  # 합격자인 경우 pass.html로 리다이렉트
+        else:
+            return render(request, 'recruit/fail.html')  # 불합격자인 경우 fail.html로 리다이렉트
+    except Applicant.DoesNotExist:
+        return render(request, "recruit/status.html", {"not_in_recruitment_period": True})
