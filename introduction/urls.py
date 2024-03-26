41dd8172a65a620a0204eb46ee9f851a225d3896
raon <raon-yang@naver.com>
2024-03-26 14:39:46 +0900

    [raon] 0326 origanize backend code

--- introduction/urls.py
+++ introduction/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-# from .views import Introduction
 from .views import introduction, bylaws, developers
+
 
 app_name = "Introduction"
 urlpatterns = [
     # url path: introduction/
-    # path('', Introduction.as_view()),
     path("", introduction, name="introduction"),
     path("bylaws/", bylaws, name="bylaws"),
     path("developers/", developers, name="developers"),
--- introduction/views.py
+++ introduction/views.py
@@ -1,25 +1,14 @@
 # backend
 from django.shortcuts import render
-# from rest_framework.views import APIView
-
-
-"""
-class Introduction(APIView):
-    def get(self, request):
-        print("get으로 호출")
-        return render(request, 'introduction/index.html')
-    
-    def post(self, request):
-        print("post로 호출")
-        return render(request, 'introduction/index.html')
-"""
 
 
 def introduction(request):
     return render(request, "introduction/brief.html")
 
+
 def bylaws(request):
     return render(request, "introduction/rules.html")
+
 
 def developers(request):
     return render(request, "introduction/dev.html")
