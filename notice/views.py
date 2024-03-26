3288624f2c8a3ec8aeaaf4a6b72dc3b7528a7a9f
raon <raon-yang@naver.com>
2024-03-26 15:19:07 +0900

    [raon] 0326 notice backend

--- notice/views.py
+++ notice/views.py
@@ -9,11 +9,11 @@ class Notice(APIView):
         attendance_link = AttendanceLink.objects.first()
         submission_link = SubmissionLink.objects.first()
         rental_link = RentalLink.objects.first()  
-        return render(request, 'notice/notice.html', {'attendance_link': attendance_link, 'submission_link': submission_link, 'rental_link': rental_link})
+        return render(request, "notice/notice.html", {"attendance_link": attendance_link, "submission_link": submission_link, "rental_link": rental_link})
 
     def post(self, request):
         print("post로 호출")
         attendance_link = AttendanceLink.objects.first()
         submission_link = SubmissionLink.objects.first()
         rental_link = RentalLink.objects.first()  
-        return render(request, 'notice/notice.html', {'attendance_link': attendance_link, 'submission_link': submission_link, 'rental_link': rental_link})
+        return render(request, "notice/notice.html", {"attendance_link": attendance_link, "submission_link": submission_link, "rental_link": rental_link})
