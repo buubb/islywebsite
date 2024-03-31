96480c890247422b60a55ca274fe010eadece420
djatjfdls <djatjfdls@gmail.com>
2024-04-01 02:03:49 +0900

    디자인수정_최종(1)

--- static/login/js/new3.js
+++ static/login/js/new3.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var passwordInput = document.getElementById("password");
     var loginButton = document.getElementById("login-button");
     var spanElements = document.querySelectorAll(".login-box button span");
-    var eyesIcon = document.querySelector('.user-box .eyes');
+    var eyesIcon = document.querySelector('.showHidePw');
 
 
     // 이전 페이지의 URL 파라미터 제거
@@ -40,19 +40,19 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     function togglePasswordVisibility() {
-        if (passwordInput.value.trim() !== "") {
-            eyesIcon.classList.toggle('active');
-            if (eyesIcon.classList.contains('active')) {
-                eyesIcon.querySelector('i').classList.remove('fa-eye');
-                eyesIcon.querySelector('i').classList.add('fa-eye-slash');
-                passwordInput.setAttribute('type', 'text');
-            } else {
-                eyesIcon.querySelector('i').classList.remove('fa-eye-slash');
-                eyesIcon.querySelector('i').classList.add('fa-eye');
-                passwordInput.setAttribute('type', 'password');
-            }
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            eyesIcon.classList.add('active');
+            eyesIcon.classList.remove('uil-eye-slash');
+            eyesIcon.classList.add('uil-eye');
+        } else {
+            passwordInput.type = "password";
+            eyesIcon.classList.remove('active');
+            eyesIcon.classList.remove('uil-eye');
+            eyesIcon.classList.add('uil-eye-slash');
         }
     }
+    
 
     checkInputs();
 });
