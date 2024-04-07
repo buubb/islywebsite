c960d6d9e239f349bc0ecafbf313c6f2090a8dbf
raon <raon-yang@naver.com>
2024-04-08 04:49:10 +0900

    [raon] 0408 notice backend

--- notice/views.py
+++ notice/views.py
@@ -1,18 +1,18 @@
 # backend
 from django.shortcuts import render
-from .models import AttendanceLink, SubmissionLink, RentalLink, Rental
+from .models import AttendanceLink, SubmissionLink, RentalLink, Lecture
 
 
 def notice(request):
     attendance_link = AttendanceLink.objects.last()
     submission_link = SubmissionLink.objects.last()
     rental_link = RentalLink.objects.last()
-    rental = Rental.objects.last()
+    lectures = Lecture.objects.all()
 
     context = {
         "attendance_link": attendance_link,
         "submission_link": submission_link,
         "rental_link": rental_link,
-        "rental": rental,
+        "lectures": lectures,
     }
     return render(request, "notice/notice.html", context)
