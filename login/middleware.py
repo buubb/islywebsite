1bd42763562b15b48ae8532601bf3d5f8a6cb39c
djatjfdls <20211076@sungshin.ac.kr>
2024-05-13 01:20:46 +0900

    [tjfdls] 0513 migrate error

--- login/middleware.py
+++ login/middleware.py
@@ -13,13 +13,13 @@ class CsrfFailureMiddleware:
             return render(request, 'mainpage/index.html')
         return response
     
-class Handle404Middleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+# class Handle404Middleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
-        response = self.get_response(request)
-        # 404 에러가 발생하면 메인 페이지로 리디렉션
-        if response.status_code == 404:
-            return render(request, 'mainpage/index.html')
-        return response
+#     def __call__(self, request):
+#         response = self.get_response(request)
+#         # 404 에러가 발생하면 메인 페이지로 리디렉션
+#         if response.status_code == 404:
+#             return render(request, 'mainpage/index.html')
+#         return response
