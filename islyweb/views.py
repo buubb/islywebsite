b61ed8cee2168279501078ef2252b91c73fe8ced
Yubin <qls773322@naver.com>
2024-05-16 02:00:27 +0900

    [yubin] 404page/url update

--- islyweb/views.py
+++ islyweb/views.py
@@ -1,6 +1,8 @@
 # backend
 from django.shortcuts import render
 from rest_framework.views import APIView
+from django.template import RequestContext
+from django.http import HttpResponseNotFound, HttpResponseServerError
 
 class Main(APIView):
     def get(self, request):
@@ -11,3 +13,15 @@ class Main(APIView):
         print("post로 호출")
         return render(request, 'islyweb/index.html')
     
+# 400 Error
+def bad_request(request, exception):
+    return render(request, '404page.html', status=400)
+
+# 404 Error
+def page_not_found(request, exception):
+    return render(request, '404page.html')
+
+# 500 Error
+def server_error(request):
+    return render(request, '404page.html', status=500)
+
