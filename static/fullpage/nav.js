f8d8380317cf3cbca9814e50677025873ffaa9ff
Yubin <qls773322@naver.com>
2024-05-18 01:33:43 +0900

    [yubin] 백엔드 마무리, 파트너쉽 추가

--- static/fullpage/nav.js
+++ static/fullpage/nav.js
@@ -7,9 +7,17 @@ $(document).on("click", function(event) {
       if($(".depth_1").is(":visible") && !$(event.target).closest(".depth_1").length) {
           $(".depth_1").slideUp();
       }
+      if($(".depth_2").is(":visible") && !$(event.target).closest(".depth_2").length) {
+        $(".depth_2").slideUp();
+    }
   });
 
 $(".aboutdrop").click(function(event){
-        event.stopPropagation(); // 클릭 이벤트가 부모로 전파되지 않도록 함
-        $(".depth_1").slideToggle();
+    event.stopPropagation(); // 클릭 이벤트가 부모로 전파되지 않도록 함
+    $(".depth_1").slideToggle();
   });
+
+$(".aboutdrop2").click(function(event){
+    event.stopPropagation(); // 클릭 이벤트가 부모로 전파되지 않도록 함
+    $(".depth_2").slideToggle();
+});
