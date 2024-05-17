082a5f5c7c113c4a473c2be8d9706f2dbafc4e8b
djatjfdls <20211076@sungshin.ac.kr>
2024-05-17 15:21:48 +0900

    [tjfdls] 0517 세부수정

--- login/views.py
+++ login/views.py
@@ -68,8 +68,14 @@ class CheckLogin(APIView):
         else:
             return render(request, 'login/check.html', {'form': form})
 
-@login_required
+# @login_required
 def password_edit_view(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return render(request, 'login/change_password.html')
+        else:
+            return render(request, 'mainpage/index.html')
+
     if request.method == 'POST':
         password_change_form = CustomPasswordChangeForm(request.user, request.POST)
         if password_change_form.is_valid():
