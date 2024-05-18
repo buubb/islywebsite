04ef62bf27478391905d4692d2c1a859979cfa8b
raon <raon-yang@naver.com>
2024-05-19 02:07:20 +0900

    [raon] 0519 back to top button

---
+++ static/volunteer/js/back_to_top.js
@@ -0,0 +1,20 @@
+// back_to_top.js
+
+(function ($) {
+    "use strict";
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+})(jQuery);
+
